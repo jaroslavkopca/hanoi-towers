ade8e943727addee358fcf04d32b237063545dfe
Jaroslav Kopča <kopcajar@fel.cvut.cz>
2023-12-17 22:02:23 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Semestralka.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Semestralka.dir/main.cpp.o"
+  "CMakeFiles/Semestralka.dir/main.cpp.o.d"
+  "Semestralka"
+  "Semestralka.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Semestralka.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Semestralka.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Semestralka.
+# This may be replaced when dependencies are built.
